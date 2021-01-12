5fbdd094a9067fc610547ca2822ddac85c38bf15
Nadya Moisseeva <nmoisseeva@eos.ubc.ca>
2021-01-11 20:46:44 -0800

    gitignore edits

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cwipp-nmoisseeva",
+    name="cwipp",
     version="0.0.1",
     author="Nadya Moisseeva",
     author_email="nmoisseeva@eoas.ubc.ca",
